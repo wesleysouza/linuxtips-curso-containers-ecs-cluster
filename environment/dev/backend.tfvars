c19b6bd2aa948daf2b9e95be1b0b5ba475e393fd
wesleysouza <wesleyosou@hotmail.com>
2025-03-18 19:44:54 -0300

    feat: project basic config

---
+++ environment/dev/backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "w-tf-aca-statefiles"
+key    = "ecs/dev/state"
+region = "us-east-1"
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+  
+}
+
+variable "region" {
+  description = "Region of AWS"
+}
+
+variable "ssm_vpc_id" {}
+
+variable "ssm_public_subnet_1" {}
+
+variable "ssm_public_subnet_2" {}
+
+variable "ssm_public_subnet_3" {}
+
+variable "ssm_private_subnet_1" {}
+
+variable "ssm_private_subnet_2" {}
+
+variable "ssm_private_subnet_3" {}
